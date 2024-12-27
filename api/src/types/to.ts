c63ddb67e3fe9ffc27fc7684cb3ccb77a1aee052
Abdur Rafay <abdurrafay0123@gmail.com>
2024-12-28 02:38:51 +0530

    Types for Message to Engine added

--- api/src/types/to.ts
+++ api/src/types/to.ts
@@ -1,12 +1,24 @@
-import { CREATE_ORDER } from "."
+import { CANCEL_ORDER, CREATE_ORDER, GET_OPEN_ORDERS } from "."
 
 export type MessageToEngine = {
     type: typeof CREATE_ORDER,
     data: {
         market: string,
-        side: "buy"|"sell",
-        quantity: number,
-        price: number,
+        price: string,
+        quantity: string,
+        side: "buy" | "sell",
         userId: string
+    }
+} | {
+    type: typeof CANCEL_ORDER,
+    data: {
+        orderId: string,
+        market: string,
+    }
+} | {
+    type: typeof GET_OPEN_ORDERS,
+    data: {
+        userId: string,
+        market: string,
     }
 }
