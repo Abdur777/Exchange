722fafcb97d0cf963e656dfb0d6cbc0884182a35
Abdur Rafay <abdurrafay0123@gmail.com>
2024-12-28 02:33:11 +0530

    Delete and Get order end point added

--- api/dist/routes/order.js
+++ api/dist/routes/order.js
@@ -12,12 +12,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.orderRouter = void 0;
 const express_1 = require("express");
 const RedisManager_1 = require("../RedisManager");
+const types_1 = require("../types");
 exports.orderRouter = (0, express_1.Router)();
 exports.orderRouter.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { market, price, quantity, side, userId } = req.body;
     console.log({ market, price, quantity, side, userId });
     const response = yield RedisManager_1.RedisManager.getInstance().sendAndAwait({
-        type: "CREATE_ORDER",
+        type: types_1.CREATE_ORDER,
         data: {
             market,
             price,
@@ -26,5 +27,26 @@ exports.orderRouter.post("/", (req, res) => __awaiter(void 0, void 0, void 0, fu
             userId
         }
     });
-    res.send(response);
+    res.json(response.payload);
+}));
+exports.orderRouter.delete("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { orderId, market } = req.body;
+    const response = yield RedisManager_1.RedisManager.getInstance().sendAndAwait({
+        type: types_1.CANCEL_ORDER,
+        data: {
+            market,
+            orderId
+        }
+    });
+    res.json(response.payload);
+}));
+exports.orderRouter.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield RedisManager_1.RedisManager.getInstance().sendAndAwait({
+        type: types_1.GET_OPEN_ORDERS,
+        data: {
+            userId: req.query.userId,
+            market: req.query.market
+        }
+    });
+    res.json(response.payload);
 }));
--- api/src/routes/order.ts
+++ api/src/routes/order.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { RedisManager } from "../RedisManager";
+import { CANCEL_ORDER, CREATE_ORDER, GET_OPEN_ORDERS } from "../types";
 
 export const orderRouter = Router();
 
@@ -7,7 +8,7 @@ orderRouter.post("/",async (req,res)=>{
     const {market, price, quantity, side, userId} = req.body;
     console.log({market, price, quantity, side, userId});
     const response = await RedisManager.getInstance().sendAndAwait({
-        type: "CREATE_ORDER",
+        type: CREATE_ORDER,
         data: {
             market, 
             price, 
@@ -16,5 +17,29 @@ orderRouter.post("/",async (req,res)=>{
             userId
         }
     });
-    res.send(response);
+    res.json(response.payload);
+})
+
+
+orderRouter.delete("/", async (req,res)=> {
+   const { orderId, market } = req.body;
+    const response = await RedisManager.getInstance().sendAndAwait({
+        type: CANCEL_ORDER,
+        data: {
+            market,
+            orderId
+        }
+    })
+    res.json(response.payload)
+})
+
+orderRouter.get("/", async (req,res)=>{
+    const response = await RedisManager.getInstance().sendAndAwait({
+        type: GET_OPEN_ORDERS,
+        data: {
+            userId: req.query.userId as string,
+            market: req.query.market as string
+        }
+    })
+    res.json(response.payload)
 })
