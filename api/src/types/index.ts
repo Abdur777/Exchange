2b2637e8d6eaa17981274d955090872e2a410b58
Abdur Rafay <abdurrafay0123@gmail.com>
2024-12-28 02:37:50 +0530

    Types for MessageFromOrderbook added

--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -1,1 +1,35 @@
 export const CREATE_ORDER = "CREATE_ORDER";
+export const CANCEL_ORDER = "CANCEL_ORDER";
+export const GET_OPEN_ORDERS = "GET_OPEN_ORDERS";
+
+
+export type MessageFromOrderbook = {
+    type: "ORDER_PLACED"
+    payload: {
+        orderId: string,
+        executedQty: number,
+        fills: [
+            {
+                price: string,
+                qty: number,
+                tradeId: string
+            }
+        ]
+    }
+} | {
+    type: "ORDER_CANCELLED",
+    payload: {
+        orderId: string,
+        executedQty: number,
+        remainingQty: number
+    }
+} | {
+    type: "OPEN_ORDERS",
+    payload: {
+        orderId: string,
+        remainingQty: number
+        price: string,
+        side: 'buy' | 'sell',
+        userId: string
+    }
+}
