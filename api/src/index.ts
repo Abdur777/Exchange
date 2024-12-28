4e8404d32a60c40ae5a97be538366e8ad22e8b35
Abdur Rafay <abdurrafay0123@gmail.com>
2024-12-28 23:51:24 +0530

    added router for depth, trades

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import { orderRouter } from "./routes/order";
+import { depthRouter } from "./routes/depth";
+import { tradesRouter } from "./routes/trades";
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/v1/order",orderRouter);
+app.use("api/v1/depth",depthRouter);
+app.use("api/v1/trades",tradesRouter)
 
 app.listen(3000,()=>{
     console.log("listing at port 3000");
