2d451355a9e73e27991ae62d7a1e5c8488716266
Abdur Rafay <abdurrafay0123@gmail.com>
2025-01-02 00:57:09 +0530

    types for cancel, open orders, depth

--- engine/src/types/toApi.ts
+++ engine/src/types/toApi.ts
@@ -1,3 +1,5 @@
+import { Order } from "../trade/OrderBook"
+
 export type MessageToApi = {
     type: "ORDER_PLACED",
     payload: {
@@ -8,5 +10,22 @@ export type MessageToApi = {
             qty: number,
             tradeId: number
         }[]
+    }
+} | {
+    type: "ORDER_CANCELLED",
+    payload: {
+        orderId: string,
+        executedQty: number,
+        remainingQty: number
+    }
+} | {
+    type: "OPEN_ORDERS",
+    payload: Order []
+} | {
+    type: "DEPTH",
+    payload: {
+        market: string,
+        bids: [string,string][],
+        asks: [string,string][]
     }
 }
