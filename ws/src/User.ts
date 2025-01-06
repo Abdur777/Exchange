f98df175b1a84ab250fcedce781188cec09feb7b
Abdur Rafay <abdurrafay0123@gmail.com>
2025-01-06 20:13:18 +0530

    User class init

--- ws/src/User.ts
+++ ws/src/User.ts
@@ -1,4 +1,7 @@
 import { WebSocket } from "ws";
+import { IncomingMessage, SUBSCRIBE, UNSUBSCRIBE } from "./types/in";
+import { SubscriptionManager } from "./SubscriptionManager";
+import { OutgoingMessage } from "./types/out";
 
 export class User {
     private id: string
@@ -7,5 +10,23 @@ export class User {
     constructor(id: string, ws: WebSocket){
         this.id = id;
         this.ws = ws;
+        this.addListeners();
+    }
+
+    private addListeners(){
+        this.ws.on("message", (message: string) => {
+            const parsedMessage: IncomingMessage = JSON.parse(message);
+            if (parsedMessage.method === SUBSCRIBE) {
+                parsedMessage.params.forEach(s => SubscriptionManager.getInstance().subscribe(this.id, s));
+            }
+            if (parsedMessage.method === UNSUBSCRIBE) {
+                parsedMessage.params.forEach(s => SubscriptionManager.getInstance().unsubscribe(this.id, parsedMessage.params[0]));
+            }
+        });
+    }
+
+    emit(message: OutgoingMessage){
+        const parsedMessage = JSON.stringify(message);
+        this.ws.send(parsedMessage);
     }
 }
