f003caadcbc8e85a6f3f32b5001b5743d2ddd9fa
Abdur Rafay <abdurrafay0123@gmail.com>
2025-01-07 23:38:11 +0530

    frontend init

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Markets } from "./components/Markets";
+
+export default function Home() {
+  return (
+    <main>
+      <Markets />
+    </main>
+  );
+}
