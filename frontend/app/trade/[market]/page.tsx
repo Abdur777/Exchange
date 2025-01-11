c18b252c4324ec5c92ff545885fb254954b0c4b3
Abdur Rafay <abdurrafay0123@gmail.com>
2025-01-12 01:50:53 +0530

    parent comp added

---
+++ frontend/app/trade/[market]/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import MarketBar from "@/app/components/MarketBar";
+import { useParams } from "next/navigation";
+
+
+export default function Page (){
+
+    const {market} = useParams()
+
+    return <div className="flex flex-row flex-1 px-4 gap-2">
+    <div className="flex flex-col flex-1 gap-2">
+        <MarketBar market={market as string} />
+        <div className="flex flex-row h-[920px] border-y border-slate-800">
+            <div className="flex flex-col flex-1">
+                TradeView
+            </div>
+            <div className="flex flex-col w-[250px] overflow-hidden">
+                Depth
+            </div>
+        </div>
+    </div>
+    <div className="w-[10px] flex-col border-slate-800 border-l"></div>
+    <div>
+        <div className="flex flex-col w-[250px]">
+            Swap
+        </div>
+    </div>
+</div>
+}
