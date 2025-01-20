bb091e44714be9bc75e2cb0a18f8d3da23b4f1f1
Abdur Rafay <abdurrafay0123@gmail.com>
2025-01-20 23:01:04 +0530

    realtime bids and ask logic

--- frontend/app/components/depth/Depth.tsx
+++ frontend/app/components/depth/Depth.tsx
@@ -1,12 +1,64 @@
 import { SignalingManager } from "@/app/utils/SignalingManager"
 import { useEffect, useState } from "react"
+import { BidTable } from "./BidTable"
+import AskTable from "./AskTable"
 
 export function Depth({ market }: {market: string}) {
     const [bids, setBids]  = useState<[string,string][]>()
     const [asks, setAsks]  = useState<[string,string][]>()
     useEffect(() => {
         SignalingManager.getInstance().registerCallBack("depth", (data: any) => {
-            console.log("depth has been updated",data);
+            // console.log("depth has been updated",data);
+            setBids(( prev ) => {
+                const bidsAfterUpdate = [...(prev || [])];
+
+                for (let i = 0; i < bidsAfterUpdate.length; i++) {
+                    for (let j = 0; j < data.bids.length; j++)  {
+                        if (bidsAfterUpdate[i][0] === data.bids[j][0]) {
+                            bidsAfterUpdate[i][1] = data.bids[j][1];
+                            if (Number(bidsAfterUpdate[i][1]) === 0) {
+                                bidsAfterUpdate.splice(i, 1);
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                for (let j = 0; j < data.bids.length; j++)  {
+                    if (Number(data.bids[j][1]) !== 0 && !bidsAfterUpdate.map(x => x[0]).includes(data.bids[j][0])) {
+                        bidsAfterUpdate.push(data.bids[j]);
+                        break;
+                    }
+                }
+                bidsAfterUpdate.sort((x, y) => Number(y[0]) > Number(x[0]) ? -1 : 1);
+                return bidsAfterUpdate; 
+                
+            })
+
+            setAsks((originalAsks) => {
+                const asksAfterUpdate = [...(originalAsks || [])];
+
+                for (let i = 0; i < asksAfterUpdate.length; i++) {
+                    for (let j = 0; j < data.asks.length; j++)  {
+                        if (asksAfterUpdate[i][0] === data.asks[j][0]) {
+                            asksAfterUpdate[i][1] = data.asks[j][1];
+                            if (Number(asksAfterUpdate[i][1]) === 0) {
+                                asksAfterUpdate.splice(i, 1);
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                for (let j = 0; j < data.asks.length; j++)  {
+                    if (Number(data.asks[j][1]) !== 0 && !asksAfterUpdate.map(x => x[0]).includes(data.asks[j][0])) {
+                        asksAfterUpdate.push(data.asks[j]);
+                        break;
+                    }
+                }
+                asksAfterUpdate.sort((x, y) => Number(y[0]) > Number(x[0]) ? 1 : -1);
+                return asksAfterUpdate; 
+            });
         },`DEPTH-${market}`) 
         SignalingManager.getInstance().sendMessage({"method":"SUBSCRIBE","params":[`depth.${market}`]});
         return () => {
@@ -25,13 +77,13 @@ export function Depth({ market }: {market: string}) {
                 </div>
                 <div className="flex flex-col no-scrollbar h-full flex-1 overflow-y-auto font-sans">
                     <div className="flex flex-col flex-1">
-                        AskTable
+                        {asks && <AskTable asks={asks}/>}
                     </div>
                     <div className="flex flex-col flex-0 z-20 snap-center bg-baseBackgroundL1 px-3 py-1 sticky bottom-0">
                         Ticker
                     </div>
                     <div className="flex flex-col flex-1">
-                        BidTable
+                        {bids && <BidTable bids={bids} />}
                     </div>
                 </div>
                 <div className="relative mx-3 my-1 overflow-hidden">BuySellPressure%</div>
