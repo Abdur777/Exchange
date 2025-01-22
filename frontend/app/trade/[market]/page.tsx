5132d8ff0ba7a6ddf254695121256de365059707
Abdur Rafay <abdurrafay0123@gmail.com>
2025-01-22 18:05:38 +0530

    Price provide added

--- frontend/app/trade/[market]/page.tsx
+++ frontend/app/trade/[market]/page.tsx
@@ -3,12 +3,14 @@ import { Depth } from "@/app/components/depth/Depth";
 import MarketBar from "@/app/components/MarketBar";
 import { SwapUI } from "@/app/components/SwapUI";
 import TradeView from "@/app/components/TradeView";
+import { PriceProvider } from "@/app/contexts/PriceContext";
 import { useParams } from "next/navigation";
 
 export default function Page() {
   const { market } = useParams();
 
   return (
+    <PriceProvider>  
     <div className="flex flex-row flex-1 px-4 gap-2">
       <div className="flex flex-col flex-1 gap-2">
         <MarketBar market={market as string} />
@@ -36,5 +38,6 @@ export default function Page() {
         <SwapUI market={market as string}/>
        </div>
     </div>
+    </PriceProvider>
   );
 }
