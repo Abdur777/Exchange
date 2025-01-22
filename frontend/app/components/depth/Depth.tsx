5909aef05ca7fbf15d0be6d98c3263c925a4b299
Abdur Rafay <abdurrafay0123@gmail.com>
2025-01-23 00:27:18 +0530

    UI updates

--- frontend/app/components/depth/Depth.tsx
+++ frontend/app/components/depth/Depth.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import { BidTable } from "./BidTable"
 import AskTable from "./AskTable"
 import { usePriceContext } from "@/app/contexts/PriceContext"
+import { getDepth } from "@/app/utils/httpClient"
 
 export function Depth({ market }: {market: string}) {
     const [bids, setBids]  = useState<[string,string][]>()
@@ -61,7 +62,11 @@ export function Depth({ market }: {market: string}) {
                 asksAfterUpdate.sort((x, y) => Number(y[0]) > Number(x[0]) ? 1 : -1);
                 return asksAfterUpdate; 
             });
-        },`DEPTH-${market}`) 
+        },`DEPTH-${market}`)
+        getDepth().then((res)=>{
+            setBids(res.asks);
+            setAsks(res.bids)
+        })
         SignalingManager.getInstance().sendMessage({"method":"SUBSCRIBE","params":[`depth.${market}`]});
         return () => {
             SignalingManager.getInstance().sendMessage({"method":"UNSUBSCRIBE","params":[`depth.${market}`]});
@@ -73,26 +78,26 @@ export function Depth({ market }: {market: string}) {
         <div className="flex flex-col grow overflow-y-hidden">
             <div className="flex flex-col h-full grow overflow-x-hidden">
                 <div className="flex flex-row gap-3 px-3 py-2">
-                    <p className="font-medium text-baseTextHighEmphasis w-1/3 text-xs font-medium text-sm font-inter">Price (USDC)</p>
-                    <button type="button" className="font-medium text-sm font-inter transition-opacity hover:opacity-80 hover:cursor-pointer h-auto w-1/3 truncate text-right text-xs text-baseTextMedEmphasis">Size (SOL)</button>
-                    <button type="button" className="font-medium text-sm font-inter transition-opacity hover:opacity-80 hover:cursor-pointer h-auto w-1/3 truncate text-right text-xs text-baseTextMedEmphasis">Total (SOL)</button>
+                    <p className="font-semibold w-1/3 text-xs font-inter">Price (USDC)</p>
+                    <button type="button" className="font-semibold text-sm font-inter transition-opacity hover:opacity-80 hover:cursor-pointer h-auto w-1/3 truncate text-right text-xs text-[#969FAF]">Size (SOL)</button>
+                    <button type="button" className="font-semibold text-sm font-inter transition-opacity hover:opacity-80 hover:cursor-pointer h-auto w-1/3 truncate text-right text-xs text-[#969FAF]">Total (SOL)</button>
                 </div>
                 <div className="flex flex-col no-scrollbar h-full flex-1 overflow-y-auto font-sans">
                     <div className="flex flex-col flex-1">
                         {asks && <AskTable asks={asks}/>}
                     </div>
-                    <div className={`flex flex-col flex-0 z-20 snap-center bg-baseBackgroundL1 px-3 py-1 sticky bottom-0 ${
+                    <div className={`flex flex-col flex-0 z-20 snap-center bg-baseBackgroundL1 px-3 py-1 sticky bottom-0 font-inter ${
                     Number(ticker?.priceChange) > 0
                         ? "text-green-500"
                         : "text-red-500"
                     }`}>
-                        {ticker?.lastPrice}
+                       <p className="font-semibold tabular-nums">{ticker?.lastPrice}</p>
                     </div>
                     <div className="flex flex-col flex-1">
                         {bids && <BidTable bids={bids} />}
                     </div>
                 </div>
-                <div className="relative mx-3 my-1 overflow-hidden">BuySellPressure%</div>
+                {/* <div className="relative mx-3 my-1 overflow-hidden">BuySellPressure%</div> */}
             </div>
         </div>
     </div>
