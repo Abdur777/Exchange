35899cde6b8d4ebd4bc6c4cf29fc33f0958bc88e
Abdur Rafay <abdurrafay0123@gmail.com>
2025-01-30 01:13:08 +0530

    Markets comp imported in page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,6 @@ import Markets from "./markets/page"
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col overflow-auto bg-baseBackgroundL0 text-baseTextHighEmphasis">
-      <div className="flex flex-col mx-auto w-full max-w-7xl px-3 sm:px-6">
         <Markets/>
-      </div>
-    </div>
   );
 }
